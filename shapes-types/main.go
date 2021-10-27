651695ea2a6e47d538676298dbb1751bde7b45b5
Kody Low <kodylow7@gmail.com>
2021-10-27 13:22:48 -0700

    finished course

---
+++ shapes-types/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	height float64
+	base float64
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength*s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return t.base*t.height/2
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func main() {
+	s := square{sideLength: 4}
+
+	printArea(s)
+}
